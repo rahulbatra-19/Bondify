8ce307a3fe18e0610eb17ac8822cab9c8c5bccc9
rahulbatra-19 <batrarahul409@example.com>
2023-08-12 16:10:08 +0530

    Created home Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import { getPosts } from '../api';
+import { Home } from '../pages';
+
 function App() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+    const response = await getPosts();
+    console.log('response', response);
+    }
+    fetchPosts();
+  },[]);
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,50 @@
+import styles from '../styles/home.module.css';
+export const Home = () => {
+    return (
+        <div className={styles.postsList}>
+            <div className={styles.postWrapper}>
+                <div className={styles.postHeader}>
+                    <div className={styles.postAvatar}>
+                        <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?size=626&ext=jpg"
+                            alt="user-pic"
+                        />
+                   <div>
+                    <span className={styles.postAuthor}>Rahul</span>
+                    <span className={styles.postTime}> a minute ago</span>
+                </div>
+            </div>
+            <div className={styles.postContent}> Post Content</div>
+            <div className={styles.postActions}>
+                <div className={styles.postLike}>
+                    <img src="https://img.icons8.com/?size=2x&id=16076&format=png"
+                        alt="" />
+                    <span>5</span>
+                </div>
+                <div className={styles.postCommentsIcon}>
+                    <img src="https://img.icons8.com/?size=2x&id=jOjH1Mt48Fp1&format=png
+                    "
+                        alt="comments-icon"
+                    />
+                    <span>2</span>
+                </div>
+            </div>
+            <div className={styles.postCommentBox}>
+                <input placeholder="Start typing a comment"/>
+            </div>
+            <div className={styles.postCommentList}>
+                <div className={styles.postCommentItem}>
+                    <div className={styles.postCommentHeader}>
+                        <span className={styles.postCommentAuthor}>Bill</span>
+                        <span className={styles.postCommentTime}>a minute ago</span>
+                        <span className={styles.postCommentLikes}>22</span>
+                    </div>
+
+                    <div className={styles.postCommentContent}> Random comment</div>
+                </div>
+            </div>
+                </div>
+            </div>
+            </div>
+                
+    );
+};
