3b33470263d95763305fcd21b6f331378751a2a2
rahulbatra-19 <batrarahul409@example.com>
2023-08-16 15:02:37 +0530

    Configured navbar features

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,53 @@
 import { Link } from 'react-router-dom';
 import styles from '../styles/navbar.module.css';
+import { useAuth } from '../hooks';
 const Navbar = () => {
-    return (
-        <div className={styles.nav}>
-            <div className={styles.leftDiv}>
-                <Link to="/">
-                    <img alt='logo' src='https://tse2.mm.bing.net/th?id=OIP.ULrKBWOLrvVGcwWI0VOcuQHaDo&pid=Api&P=0&h=180' className={styles.webappLogo} />
-                </Link>
-            </div>
-            <div className={styles.rightNav}>
-                <div className={styles.user}>
-                    <Link to='/about'>
-                        <img src='https://img.freepik.com/free-icon/user_318-159711.jpg?size=626&ext=jpg' alt='' className={styles.userDp } />
-                    </Link>
-                    <span>Rahul</span>
-                </div>
-                <div className={styles.navLinks}>
-                    <ul>
-                        <li>
-                            <Link to='/login'>Log in</Link>
-                        </li>
-                        <li>
-                            <a href='/'>Log out</a>
-                        </li>
-                        <li>
-                            <a href="/">Register</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  const auth = useAuth();
+  return (
+    <div className={styles.nav}>
+      <div className={styles.leftDiv}>
+        <Link to="/">
+          <img
+            alt="logo"
+            src="https://tse2.mm.bing.net/th?id=OIP.ULrKBWOLrvVGcwWI0VOcuQHaDo&pid=Api&P=0&h=180"
+            className={styles.webappLogo}
+          />
+        </Link>
+      </div>
+      <div className={styles.rightNav}>
+        {auth.user && (
+          <div className={styles.user}>
+            <a href="/">
+              <img
+                src="https://img.freepik.com/free-icon/user_318-159711.jpg?size=626&ext=jpg"
+                alt=""
+                className={styles.userDp}
+              />
+            </a>
+            <span>{auth.user.name}</span>
+          </div>
+        )}
+        <div className={styles.navLinks}>
+          <ul>
+            {auth.user ? (
+              <li onClick={auth.logout}>
+                Log out
+              </li>
+            ) : (
+              <>
+                <li>
+                  <Link to="/login">Log in</Link>
+                </li>
+                <li>
+                  <a href="/">Register</a>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
